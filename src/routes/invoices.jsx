9d3521f7a7970d12098b8f4381df79533abe7b5c
Flu3nt <gnedyalkov94@gmail.com>
2022-08-30 15:06:34 +0300

    Add listing for invices

--- src/routes/invoices.jsx
+++ src/routes/invoices.jsx
@@ -1,7 +1,34 @@
+import { Link } from "react-router-dom"
+import { getInvoices } from "../data"
+
 export default function Invoices() {
+  let invoices = getInvoices()
+
   return (
-    <main style={{ padding: "1rem 0" }}>
-      <h2>Invoices</h2>
-    </main>
+    <div
+      style={{
+        display: "flex",
+      }}
+    >
+      <nav
+        style={{
+          borderRight: "solid 1px",
+          padding: "1rem",
+        }}
+      >
+        {invoices.map((invoice) => (
+          <Link
+            style={{
+              borderRight: "solid 1px",
+              padding: "1rem",
+            }}
+            to={`/invoices/${invoice.number}`}
+            key={invoice.number}
+          >
+            {invoice.name}
+          </Link>
+        ))}
+      </nav>
+    </div>
   )
 }
